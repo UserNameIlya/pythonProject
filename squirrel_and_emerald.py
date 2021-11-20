cc35e6628c140a9fbd597a41b3da4eba7bf5f416
UserNameIlya <53954100+UserNameIlya@users.noreply.github.com>
2021-11-20 18:02:56 +0300

    Добавил проверку для 0

--- squirrel_and_emerald.py
+++ squirrel_and_emerald.py
@@ -1,9 +1,12 @@
 def squirrel(N):
 
     def fact(N):
-        if N == 1:
+
+        if N == 1 or N == 0:
             return 1
         else:
             return N * fact(N - 1)
 
     return (int(str(fact(N))[0]))
+
+print(squirrel(0))
