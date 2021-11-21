681cd7fc2e5a54f454188ae7693f214e87a1a4bc
UserNameIlya <53954100+UserNameIlya@users.noreply.github.com>
2021-11-21 11:12:33 +0300

    отредактировал имя функции

--- odometer.py
+++ odometer.py
@@ -1,6 +1,6 @@
-def odometr(list):
+def odometer(list):
 
     if len(list) == 2:
        return list[0] * list[1]
     else:
-        return list[0] * list[1] + odometr(list[2:])
+        return list[0] * list[1] + odometer(list[2:])
