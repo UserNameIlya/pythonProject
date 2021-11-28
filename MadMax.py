d3565cdf9b09bc193fa76c8288949ad4b0b96efc
UserNameIlya <53954100+UserNameIlya@users.noreply.github.com>
2021-11-28 18:28:27 +0300

    версия 1.1 28.11.21

--- MadMax.py
+++ MadMax.py
@@ -1,6 +1,5 @@
 def MadMax(Tele:list):
 
-    #Отсортировать массив по возрастанию
     def bubble_sort(Tele):
         for bypass in range(1, len(Tele)):
             for k in range(0,len(Tele)-bypass):
@@ -8,9 +7,7 @@ def MadMax(Tele:list):
                         Tele[k], Tele[k+1] = Tele[k+1],Tele[k]
         return Tele
 
-    #Сортируем вторую часть перестоновкой
     def reverse_list(Tele):
-        #Находим центр
         def get_center_index(Tele):
             center_index = len(Tele)//2
             return center_index
@@ -23,9 +20,6 @@ def MadMax(Tele:list):
                         Tele[k], Tele[k+1] = Tele[k+1],Tele[k]
         return Tele
 
-    #вернуть Tele
     bubble_sort(Tele)
     reverse_list(Tele)
     return Tele
-
-print(MadMax([2,1,7,4,5,6,3]))
