be25db02563953d397241cd86e867811be0cf240
UserNameIlya <53954100+UserNameIlya@users.noreply.github.com>
2021-11-28 18:41:20 +0300

    версия 1.2 28.11.21

--- MadMax.py
+++ MadMax.py
@@ -1,25 +1,25 @@
-def MadMax(Tele:list):
+def MadMax(N,Tele:list):
 
-    def bubble_sort(Tele):
-        for bypass in range(1, len(Tele)):
-            for k in range(0,len(Tele)-bypass):
+    def bubble_sort(N,Tele):
+        for bypass in range(1, N):
+            for k in range(0,N-bypass):
                 if Tele[k] > Tele [k+1]:
                         Tele[k], Tele[k+1] = Tele[k+1],Tele[k]
         return Tele
 
-    def reverse_list(Tele):
-        def get_center_index(Tele):
-            center_index = len(Tele)//2
+    def reverse_list(N,Tele):
+        def get_center_index(N):
+            center_index = N//2
             return center_index
 
         center_list = get_center_index(Tele)
 
-        for i in range(1, len(Tele)-center_list):
-            for k in range(center_list,len(Tele)-i):
+        for i in range(1,N-center_list):
+            for k in range(center_list,N-i):
                 if Tele[k] < Tele [k+1]:
                         Tele[k], Tele[k+1] = Tele[k+1],Tele[k]
         return Tele
 
-    bubble_sort(Tele)
-    reverse_list(Tele)
+    bubble_sort(N,Tele)
+    reverse_list(N,Tele)
     return Tele
