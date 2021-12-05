b840ebb6a6e307f89445020827123d9bcaf2dcda
UserNameIlya <53954100+UserNameIlya@users.noreply.github.com>
2021-12-05 17:51:03 +0300

    версия 1.0 05.12.2021

--- SynchronizingTables.py
+++ SynchronizingTables.py
@@ -3,15 +3,15 @@ def SynchronizingTables(N,ids:list,salary:list):
     tuple_stuff = {}
     tuple_salary = {}
     for num_element in range(0,N):
-        tuple_stuff[ids[num_element]] = num_element
-        tuple_salary[salary[num_element]] = num_element
+        tuple_stuff[num_element] = ids[num_element]
+        tuple_salary[num_element] = salary[num_element]
 
-    rez_stuff = sorted(tuple_stuff.items(), key= lambda para: para[0])
-    rez_salary = sorted(tuple_salary.items(), key= lambda para: para[0])
+    rez_stuff = sorted(tuple_stuff.items(), key= lambda para: (para[1],para[0]))
+    rez_salary = sorted(tuple_salary.items(), key= lambda para: para[1])
 
     for i in range(N):
-        new_salary[rez_stuff[i][1]] = rez_salary[i][0]
+        new_salary[rez_stuff[i][0]] = rez_salary[i][1]
 
-    return new_salary,rez_stuff,rez_salary
+    return new_salary
 
-print(SynchronizingTables(3,[50, 1, 1024],[90000, 20000, 100000]))
+SynchronizingTables(4,[50, 1,1, 1024],[90000, 20000,50000, 100000])
